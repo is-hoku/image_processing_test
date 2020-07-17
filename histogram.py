460764746dd89585538c46713369763c177f9f36
nono-n1I <nono6217n@gmail.com>
2020-07-17 11:51:49 +0900

    add histogram

---
+++ histogram.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read image
+img = cv2.imread("lenna_gray.jpeg").astype(np.float)
+
+# Display histogram
+plt.hist(img.ravel(), bins=255, rwidth=0.8, range=(0, 255))
+plt.savefig("histogram_1.png")
+plt.show()
