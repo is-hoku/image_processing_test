e2404466eb21d83198359f5390f07a06a8117013
nono-n1I <nono6217n@gmail.com>
2020-07-17 11:57:38 +0900

    create folder and organization

--- 3_hist/histogram.py
+++ 3_hist/histogram.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Read image
-img = cv2.imread("lenna_gray.jpeg").astype(np.float)
+img = cv2.imread("./img/lenna_gray.jpeg").astype(np.float)
 
 # Display histogram
 plt.hist(img.ravel(), bins=255, rwidth=0.8, range=(0, 255))
-plt.savefig("histogram_1.png")
+plt.savefig("./3_hist/histogram_1.png")
 plt.show()
