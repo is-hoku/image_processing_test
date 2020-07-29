6d4241112f472c7edb2dadb85b4390d7ea8c769b
nono-n1I <nono6217n@gmail.com>
2020-07-29 21:22:31 +0900

    Add inverse filter (PSF)

--- PSF_Restoration/boke.py
+++ PSF_Restoration/boke.py
@@ -3,27 +3,52 @@ import numpy as np
 # import matplotlib.pyplot as plt
 
 
-img = cv2.imread("./PSF_Deterioration/boke2.png", 0)
+def CreateImage(img, fil):
+    # 劣化画像を生成
+    # img=原画像，fil=点拡がり関数
+    fil = cv2.resize(fil, img.shape[1::-1])
 
-img = np.fft.fft2(img)
-img = np.fft.fftshift(img)
-img = 20*np.log(np.abs(img))
+    img = np.fft.fft2(img)
+    img = np.fft.fftshift(img)
+    # img = 20*np.log(np.abs(img))
 
-fil = [[1/256, 4/256, 6/256, 4/256, 1/256],
-       [4/256, 16/256, 24/256, 16/256, 4/256],
-       [6/256, 24/256, 36/256, 24/256, 6/256],
-       [4/256, 16/256, 24/256, 16/256, 4/256],
-       [1/256, 4/256, 6/256, 4/256, 1/256]]
+    fil = np.fft.fft2(fil)
+    fil = np.fft.fftshift(fil)
+    fil = 20*np.log(np.abs(fil))
+    out = img*(fil/255)
 
-fil = np.fft.fft2(fil)
-fil = np.fft.fftshift(fil)
-fil = 20*np.log(np.abs(fil))
-fil = np.where(fil == 0, 0, 1/fil)
+    out = np.fft.ifftshift(out)
+    out = np.fft.ifft2(out)
+    out = np.abs(out)
+    return out
 
-out = cv2.filter2D(img, cv2.CV_64F, fil)
 
-out = np.fft.ifftshift(out)
-out = np.fft.ifft2(out)
-out = np.abs(out)
-print(out)
-cv2.imwrite('./PSF_Restoration/boke.png', out)
+def RestoreImage(rekka, fil):
+    # 劣化画像から原画像を復元(点拡がり関数が既知)
+    # rekka=劣化画像，fil=点拡がり関数
+    fil = cv2.resize(fil, rekka.shape[1::-1])
+
+    rekka = np.fft.fft2(rekka)
+    rekka = np.fft.fftshift(rekka)
+    # rekka = 20*np.log(np.abs(rekka))
+
+    fil = np.fft.fft2(fil)
+    fil = np.fft.fftshift(fil)
+    fil = 20*np.log(np.abs(fil))
+    fil = np.where(fil == 0, 0, np.reciprocal(fil))
+    out = rekka*(fil*255)
+
+    out = np.fft.ifftshift(out)
+    out = np.fft.ifft2(out)
+    out = np.abs(out)
+    return out
+
+
+# img = cv2.imread('./img/lenna_gray.jpeg', 0)
+img = cv2.imread('./img/Parrots.jpg', 0)
+fil = cv2.imread("./img/psf1.png", 0)
+rekka = CreateImage(img, fil)
+cv2.imwrite('./PSF_Restoration/rekka.png', rekka)
+
+orig_img = RestoreImage(rekka, fil)
+cv2.imwrite('./PSF_Restoration/orig_img.png', orig_img)
