3586d73d14f71ff8ca34ac95a880b5d786ae1be7
nono-n1I <nono6217n@gmail.com>
2020-08-05 00:19:28 +0900

    fix bicubic interpolation

--- Bicubic_Interpolation/bicubic.py
+++ Bicubic_Interpolation/bicubic.py
@@ -2,40 +2,67 @@ import cv2
 import numpy as np
 
 
-def resize_bicubic(img, x, y):
+def resize_bicubic(img, ax, ay):
     H, W, C = img.shape
-    out = np.zeros((int(y*H), int(x*W), C), dtype=np.float)
-    for i in range(int(y*H)):
-        for j in range(int(x*W)):
-            for k in range(C):
-                xi, yi = int(j / x), int(i / y)
-                xi = np.minimum(xi, W - 3)
-                yi = np.minimum(yi, H - 3)
-                dx2 = (j / x) - xi
-                dy2 = (i / y) - yi
-                dx1 = dx2 + 1
-                dy1 = dy2 + 1
-                dx3 = 1 - dx2
-                dy3 = 1 - dy2
-                dx4 = 1 + dx3
-                dy4 = 1 + dy3
 
-                def h(t):
-                    at = np.abs(t)
-                    if at <= 1:
-                        t = at ** 3 - 2 * (at ** 2) + 1
-                    elif (1 < at) or (at <= 2):
-                        t = -at ** 3 + 5 * at ** 2 - 8 * at + 4
-                    else:
-                        t = 0
-                    return t
+    aH = int(ay * H)
+    aW = int(ax * W)
 
-                if xi < 2:
-                    xi = 1
-                if yi < 2:
-                    yi = 1
+    y = np.arange(aH).repeat(aW).reshape(aH, -1)
+    x = np.tile(np.arange(aW), (aH, 1))
+    y = (y / ay)
+    x = (x / ax)
+    print(y)
+    print(x)
 
-                out[i, j, k] = h(dx2)*h(dy2)*img[yi, xi, k] + h(dy2)*h(dx3)*img[yi, xi+1, k] + h(dy3)*h(dx2)*img[yi+1, xi, k] + h(dx3)*h(dx3)*img[yi+1, xi+1, k] + h(dy1)*h(dx2)*img[yi-1, xi, k] + h(dy1)*h(dx3)*img[yi-1, xi+1, k] + h(dy1)*h(dx4)*img[yi-1, xi+2, k] + h(dy2)*h(dx4)*img[yi, xi+2 ,k] + h(dy3)*h(dx4)*img[yi+1, xi+2, k] + h(dy4)*h(dx4)*img[yi+2, xi+2, k] + h(dx3)*h(dy4)*img[yi+2, xi+1, k] + h(dy4)*h(dx2)*img[yi+2, xi, k] + h(dy4)*h(dx1)*img[yi+2, xi-1, k] + h(dy3)*h(dx1)*img[yi+1, xi-1, k] + h(dy2)*h(dx1)*img[yi, xi-1, k] + h(dx1)*h(dy1)*img[yi-1, xi-1, k]
+    ix = np.floor(x).astype(np.int)
+    iy = np.floor(y).astype(np.int)
+
+    ix = np.minimum(ix, W-1)
+    iy = np.minimum(iy, H-1)
+
+    dx2 = x - ix
+    dy2 = y - iy
+    dx1 = dx2 + 1
+    dy1 = dy2 + 1
+    dx3 = 1 - dx2
+    dy3 = 1 - dy2
+    dx4 = 1 + dx3
+    dy4 = 1 + dy3
+
+    dxs = [dx1, dx2, dx3, dx4]
+    dys = [dy1, dy2, dy3, dy4]
+
+    def weight(t):
+        a = -1.
+        at = np.abs(t)
+        w = np.zeros_like(t)
+        ind = np.where(at <= 1)
+        w[ind] = ((a+2) * np.power(at, 3) - (a+3) * np.power(at, 2) + 1)[ind]
+        ind = np.where((at > 1) & (at <= 2))
+        w[ind] = (a*np.power(at, 3) - 5*a*np.power(at, 2) + 8*a*at - 4*a)[ind]
+        return w
+
+    w_sum = np.zeros((aH, aW, C), dtype=np.float32)
+    out = np.zeros((aH, aW, C), dtype=np.float32)
+
+    for j in range(-1, 3):
+        for i in range(-1, 3):
+            ind_x = np.minimum(np.maximum(ix + i, 0), W-1)
+            ind_y = np.minimum(np.maximum(iy + j, 0), H-1)
+
+            wx = weight(dxs[i+1])
+            wy = weight(dys[j+1])
+            wx = np.repeat(np.expand_dims(wx, axis=-1), 3, axis=-1)
+            wy = np.repeat(np.expand_dims(wy, axis=-1), 3, axis=-1)
+
+            w_sum += wx * wy
+            out += wx * wy * img[ind_y, ind_x]
+
+    out /= w_sum
+    out = np.clip(out, 0, 255)
+    out = out.astype(np.uint8)
+
     return out
 
 
